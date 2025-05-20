dec008e4aee79b0b26fd49350bfa825da422c616
Jakshylyk8890 <jakshylyk.ashyrmamatov@gmail.com>
2025-05-20 06:39:18 +0600

    ALLOWED_HOSTS = ['*']

--- backend/barbershop/settings.py
+++ backend/barbershop/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = 'django-insecure-jn@&kv+=q9b^l!b0!+h1o2r=f%ttp)3p=@2uh!-eusxd^=9t_k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 INSTALLED_APPS = [
